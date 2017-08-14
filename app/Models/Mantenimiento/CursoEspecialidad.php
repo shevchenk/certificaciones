f983509d320fd185c98d33a8a81ee0687632a953
thorhan <paezvallejohecthor@gmail.com>
2017-08-14 14:54:29 -0500

    modelcursoespecialidad

---
+++ app/Models/Mantenimiento/CursoEspecialidad.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Mantenimiento;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class CursoEspecialidad extends Model
+{
+    protected   $table = 'mat_cursos_especialidades';
+
+    
+
+}
