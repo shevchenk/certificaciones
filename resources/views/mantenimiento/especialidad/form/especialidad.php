ad86ca51ea6b02ef088fb89e197128ad5253cdc8
thorhan <paezvallejohecthor@gmail.com>
2017-08-14 01:07:29 -0500

    easd

--- resources/views/mantenimiento/especialidad/form/especialidad.php
+++ resources/views/mantenimiento/especialidad/form/especialidad.php
@@ -1,5 +1,5 @@
 <div class="modal" id="ModalEspecialidad" tabindex="-1" role="dialog">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
       <div class="modal-content"> 
         <div class="modal-header btn-info">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -8,37 +8,35 @@
         </div>
         <div class="modal-body"><!-- INICIO BODY-->
             <form id="ModalEspecialidadForm"><!-- INICIO FORM-->
+          <fieldset>
             <div class="form-group"><!-- INICIO FORM GROUP-->
 
-              <div class="col-md-12">              
-                <label>Especialidad</label>
-                <input type="text" class="form-control" id="txt_especialidad" name="txt_especialidad" placeholder="Especialidad">
+              <div class="col-md-12">   
+
+                <div class="col-sm-6">       
+                  <label>Especialidad</label>
+                  <input type="text" class="form-control" id="txt_especialidad" name="txt_especialidad" placeholder="Especialidad">
+                </div>
+
+                <div class="col-sm-6">
+                  <label>Certificado Especialidad</label>
+                  <input type="text" class="form-control" id="txt_certificado_especialidad" name="txt_certificado_especialidad" placeholder="Certificado Especialidad">
+                  </div>
+
               </div>
             
 
-            <div class="col-md-12">
-              <div class="form-group">
-                <label>Certificado Especialidad</label>
-                <input type="text" class="form-control" id="txt_certificado_especialidad" name="txt_certificado_especialidad" placeholder="Certificado Especialidad">
+              <div class="col-sm-6">    
+              <br>                    
+                      <label>Estado</label>
+                        <select class="form-control selectpicker show-menu-arrow" name="slct_estado" id="slct_estado">
+                          <option value='0'>Inactivo</option>
+                          <option value='1' selected>Activo</option>
+                        </select>               
               </div>
-            </div>
-
-
-
-            <div class="col-md-12"> <!-- INICIO CLASS 12 -->
-
-
-              <div class="col-sm-6">
-                <label>Estado</label>
-                  <select class="form-control" name="slct_estado" id="slct_estado">
-                    <option value='0'>Inactivo</option>
-                    <option value='1' selected>Activo</option>
-                  </select>
-              </div>
-              
-            </div> <!-- FIN CLASS 12 -->
 
           </div> <!-- FIN FORM GROUP-->
+          </fieldset>
           </form><!-- FIN FORM-->
         </div><!-- FIN BOODY-->
         <div class="modal-footer">
