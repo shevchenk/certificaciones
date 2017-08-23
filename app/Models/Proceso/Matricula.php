4f4167d12adfb0da5e30efbbfb5dfd026f7e9ac7
Renzo CB <rcapchab@gmail.com>
2017-08-23 08:39:47 -0500

    Matri

--- app/Models/Proceso/Matricula.php
+++ app/Models/Proceso/Matricula.php
@@ -22,6 +22,9 @@ class Matricula extends Model
            $al->region_id=trim( $r->region_id);
            $al->provincia_id=trim( $r->provincia_id);
            $al->distrito_id=trim( $r->distrito_id);
+           $al->direccion=trim( $r->direccion);
+           $al->referencia=trim($r->referencia);
+           $al->persona_id_updated_at=Auth::user()->id;
            $al->save();
         }else {
            $al= new Alumno;
