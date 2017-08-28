6548717aeae0b6e9fa4577b899e19a924bd696b3
Henry Morales <jakemate_cs@hotmail.com>
2017-08-28 00:16:21 -0500

    reporte NOTAS inicio

---
+++ resources/views/reporte/notas/js/notas.php
@@ -0,0 +1,98 @@
+<script type="text/javascript">
+$(document).ready(function() {
+    $(".fecha").datetimepicker({
+        format: "yyyy-mm",
+        language: 'es',
+        showMeridian: false,
+        time:false,
+        minView:3,
+        startView:3,
+        autoclose: true,
+        todayBtn: false
+    });
+    
+    $("#TableNotas").DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": false,
+        "info": true,
+        "autoWidth": false
+    });
+    
+    function DataToFilter(){
+        var fecha_ini_mat = $('#txt_fecha_ini_mat').val();
+        var fecha_fin_mat = $('#txt_fecha_fin_mat').val();
+        var fecha_inicio = $('#txt_fecha_inicio').val();
+        var fecha_final = $('#txt_fecha_final').val();
+        var data = [];
+        if ( fecha_inicio!=="" && fecha_final!=="") {
+            data.push({fecha_ini_mat:fecha_ini_mat,fecha_fin_mat:fecha_fin_mat});
+        }else if ( fecha_inicio!=="" && fecha_final!=="") {
+            data.push({fecha_inicio:fecha_inicio,fecha_final:fecha_final});
+        } else {
+            alert("Seleccione Fechas");
+        }
+        return data;
+    }
+    
+    $("#btn_generar").click(function (){
+        var data = DataToFilter();            
+        if(data.length > 0){
+            AjaxNotas.Cargar(data[0],HTMLCargarNotas);         
+        }
+    });
+
+    $(document).on('click', '#btnexport', function(event) {
+        var data = DataToFilter();
+        if(data.length > 0){
+            $(this).attr('href','AjaxDinamic/Reporte.NotasEM@getLoadNOTASExport'+'?fecha_inicio='+data[0]['fecha_inicio']+'&fecha_final='+data[0]['fecha_inicial']);            
+        }else{
+            event.preventDefault();
+        }
+    });
+
+});
+
+
+HTMLCargarNotas=function(result){
+    var html="";
+    $('#TableNotas').DataTable().destroy();
+
+    $.each(result.data,function(index,r){
+        //var curso=r.cursos.split(",");
+        //var nro=r.nro_pago_certificado.split(",");
+        //var monto=r.monto_pago_certificado.split(",");
+
+        html+="<tr id='trid_"+r.paterno+"'>"+
+            "<td>"+r.dni+"</td>"+
+            "<td>"+r.nombre+"</td>"+
+            "<td>"+r.paterno+"</td>"+
+            "<td>"+r.materno+"</td>"+
+            "<td>"+r.telefono+"</td>"+
+            "<td>"+r.email+"</td>"+
+            "<td>"+r.sucursal+"</td>"+
+            "<td>"+r.fecha_inicio+"</td>"+
+            "<td>"+r.fecha_final+"</td>"+
+            "<td>"+r.docente+"</td>"+
+            "<td>"+r.modalidad+"</td>"+
+            "<td>"+r.curso+"</td>"+
+            "<td>"+r.nota_curso+"</td>"+
+            "<td>"+r.nro_pago_certificado+"</td>"+
+            "<td>"+r.monto_pago_certificado+"</td>"+
+            "<td>"+r.nro_promocion+"</td>"+
+            "<td>"+r.monto_promocion+"</td>";
+        html+="</tr>";
+    });
+    $("#TableNotas tbody").html(html); 
+    $("#TableNotas").DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false
+    });
+};
+
+</script>
---
+++ resources/views/reporte/notas/notas.blade.php
@@ -0,0 +1,120 @@
+@extends('layout.master')  
+
+@section('include')
+@parent
+{{ Html::style('lib/datatables/dataTables.bootstrap.css') }}
+{{ Html::script('lib/datatables/jquery.dataTables.min.js') }}
+{{ Html::script('lib/datatables/dataTables.bootstrap.min.js') }}
+
+{{ Html::style('lib/bootstrap-select/dist/css/bootstrap-select.min.css') }}
+{{ Html::script('lib/bootstrap-select/dist/js/bootstrap-select.min.js') }}
+{{ Html::script('lib/bootstrap-select/dist/js/i18n/defaults-es_ES.min.js') }}
+
+{{ Html::style('lib/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css') }}
+{{ Html::script('lib/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js') }}
+{{ Html::script('lib/bootstrap-datetimepicker/js/locales/bootstrap-datetimepicker.es.js') }}
+
+@include( 'reporte.notas.js.notas_ajax' )
+@include( 'reporte.notas.js.notas' )
+@stop
+
+@section('content')
+<style>
+    .modal { overflow: auto !important; 
+    </style>
+    <section class="content-header">
+        <h1>NOTAS
+            <small>Reporte</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><i class="fa fa-sitemap"></i> Reporte</li>
+            <li class="active">NOTAS</li>
+        </ol>
+    </section>
+
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box">
+                    <form id="NotasForm">
+                        <div class="box-body table-responsive no-padding">
+                            <div class="col-sm-12">
+                                <div class="col-sm-2">
+                                    <label class="control-label">Matricula Desde:</label>
+                                    <input type="text" class="form-control fecha" placeholder="AAAA-MM" id="txt_fecha_inicial" name="txt_fecha_ini_mat" readonly=""/>
+                                </div>
+                                <div class="col-sm-2">
+                                    <label class="control-label">MatriculaHasta:</label>
+                                    <input type="text" class="form-control fecha" placeholder="AAAA-MM" id="txt_fecha_final" name="txt_fecha_fin_mat" readonly=""/>
+                                </div>
+                                <div class="col-sm-2">
+                                    <label class="control-label">Programación Desde:</label>
+                                    <input type="text" class="form-control fecha" placeholder="AAAA-MM" id="txt_fecha_inicial" name="txt_fecha_inicio" readonly=""/>
+                                </div>
+                                <div class="col-sm-2">
+                                    <label class="control-label">Programación Hasta:</label>
+                                    <input type="text" class="form-control fecha" placeholder="AAAA-MM" id="txt_fecha_final" name="txt_fecha_final" readonly=""/>
+                                </div>
+                                <div class="col-sm-1" style="padding:24px">
+                                        <span class="btn btn-primary btn-md" id="btn_generar" name="btn_generar"><i class="glyphicon glyphicon-search"></i> Buscar</span>
+                                    </div>
+                                    <div class="col-sm-1" style="padding:24px">
+                                        <a class='btn btn-success btn-md' id="btnexport" name="btnexport" href='' target="_blank"><i class="glyphicon glyphicon-download-alt"></i> Export</i></a>
+                                    </div>
+                                </div>
+                            </div><!-- .box-body -->
+                        </form><!-- .form -->
+                            <div class="box-body table-responsive no-padding">
+                                <table id="TableNotas" class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr class="cabecera">
+                                            <th>DNI</th>
+                                            <th>Nombres</th>
+                                            <th>Paterno</th>
+                                            <th>Materno</th>
+                                            <th>Teléfono</th>
+                                            <th>Email</th>
+                                            <th>ODE</th>
+                                            <th>Fecha de Inicio</th>
+                                            <th>Fecha de Fin</th>
+                                            <th>Docente</th>
+                                            <th>Modalidad</th>
+                                            <th>Curso</th>
+                                            <th>Nota del Curso</th>
+                                            <th>Nro Pago Certificado</th>
+                                            <th>Monto Pago Certificado</th>
+                                            <th>Nro Pago Promoción</th>
+                                            <th>Monto Pago Promoción</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr class="cabecera">
+                                            <th>DNI</th>
+                                            <th>Nombres</th>
+                                            <th>Paterno</th>
+                                            <th>Materno</th>
+                                            <th>Teléfono</th>
+                                            <th>Email</th>
+                                            <th>ODE</th>
+                                            <th>Fecha de Inicio</th>
+                                            <th>Fecha de Fin</th>
+                                            <th>Docente</th>
+                                            <th>Modalidad</th>
+                                            <th>Curso</th>
+                                            <th>Nota del Curso</th>
+                                            <th>Nro Pago Certificado</th>
+                                            <th>Monto Pago Certificado</th>
+                                            <th>Nro Pago Promoción</th>
+                                            <th>Monto Pago Promoción</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div><!-- .box-body -->
+                    </div><!-- .box -->
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </section><!-- .content -->
+        @stop
+
