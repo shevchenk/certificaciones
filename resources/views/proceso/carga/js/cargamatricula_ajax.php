1fe07bdbfd56eba01979e0251eef02903a8cc102
Cyberusbel <rusbelc02@gmail.com>
2017-09-12 10:19:45 -0500

    Nv Cambios

--- resources/views/proceso/carga/js/cargamatricula_ajax.php
+++ resources/views/proceso/carga/js/cargamatricula_ajax.php
@@ -17,11 +17,11 @@ var Cargar={
     Matriculas:function(evento){
         var data = new FormData($("#form_file_m")[0]);
         $.ajax({
-            url     : 'AjaxDinamic/Proceso.CargarPR@CargaMatriculas',
+            url     : 'AjaxDinamic/Proceso.CargarPR@CargarMatriculas',
             type    : 'POST',
             data    : data,
             async   : true,
-            success : function (obj) { HTMLMsg(obj); $('#btn_cargar').prop('disabled', false).html('<span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Guardar'); },
+            success : function (obj) { HTMLMsg(obj); $('#btn_cargar_m').prop('disabled', false).html('<span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Guardar'); },
             error   : function(jqXHR, textStatus, error){ alert(jqXHR.responseText); },
             cache   : false,
             contentType: false,
