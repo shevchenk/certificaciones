17a0b2fa9dd65b78636a625a50aa93ff757c0908
shevchenk <jorgeshevchenk@gmail.com>
2017-10-30 22:08:40 -0500

    actualiacion limit

--- app/Http/Controllers/Reporte/ReporteEM.php
+++ app/Http/Controllers/Reporte/ReporteEM.php
@@ -41,6 +41,8 @@ class ReporteEM extends Controller
     
     public function ExportPAE(Request $r )
     {
+        ini_set('memory_limit', '1024M');
+        set_time_limit(300);
         $renturnModel = Reporte::runExportPAE($r);
         
         Excel::create('Matricula', function($excel) use($renturnModel) {
@@ -116,6 +118,8 @@ class ReporteEM extends Controller
 
     public function ExportPAEDesglose(Request $r )
     {
+        ini_set('memory_limit', '1024M');
+        set_time_limit(300);
         $renturnModel = Reporte::runExportPAE($r);
         
         Excel::create('Matricula', function($excel) use($renturnModel) {
@@ -165,6 +169,8 @@ class ReporteEM extends Controller
 
     public function ExportPAEDes(Request $r )
     {
+        ini_set('memory_limit', '1024M');
+        set_time_limit(300);
         $renturnModel = Reporte::runExportPAE($r);
         
         Excel::create('Matricula', function($excel) use($renturnModel) {
@@ -300,14 +306,14 @@ class ReporteEM extends Controller
     {
         $renturnModel = Reporte::runExportIndiceMat($r);
         
-        Excel::create('Matricula', function($excel) use($renturnModel) {
+        Excel::create('Matricula', function($excel) use($renturnModel,$r) {
 
         $excel->setTitle('Reporte de Matriculas')
               ->setCreator('Jorge Salcedo')
               ->setCompany('JS Soluciones')
               ->setDescription('Matrícula PAE o Seminarios');
 
-        $excel->sheet('Matricula', function($sheet) use($renturnModel) {
+        $excel->sheet('Indice_Mat', function($sheet) use($renturnModel,$r) {
             $sheet->setOrientation('landscape');
             $sheet->setPageMargin(array(
                 0.25, 0.30, 0.25, 0.30
@@ -321,8 +327,13 @@ class ReporteEM extends Controller
                 )
             ));
 
+            $titulo=" Seminario";
+            if( $r->tipo_curso==1 ){
+                $titulo=" PAE";
+            }
+
             $sheet->cell('A1', function($cell) {
-                $cell->setValue('REPORTE DE MATRICULAS PAE');
+                $cell->setValue('REPORTE DE INDICE DE MATRICULAS -'.$titulo);
                 $cell->setFont(array(
                     'family'     => 'Bookman Old Style',
                     'size'       => '20',
@@ -338,72 +349,21 @@ class ReporteEM extends Controller
 
             //$sheet->setWidth($renturnModel['length']);
 
-            for ($i=0; $i < count($renturnModel['cabecera1']); $i++) { 
-                $campoinicial= explode(":",$renturnModel['cabecantLetra'][$i]);
-                $sheet->cell($campoinicial[0], function($cell) use($renturnModel,$i) {
-                    $cell->setValue($renturnModel['cabecera1'][$i]);
-                });
-                $sheet->mergeCells($renturnModel['cabecantLetra'][$i]);
-                $sheet->cells($renturnModel['cabecantLetra'][$i], function($cells) {
-                    $cells->setBorder('solid', 'none', 'none', 'solid');
-                    $cells->setAlignment('center');
-                    $cells->setValignment('center');
-                });
-            }
-
-            $sheet->row( 4, $renturnModel['cabecera2'] );
+            $sheet->row( 3, $renturnModel['cabecera2'] );
 
             $data=json_decode(json_encode($renturnModel['data']), true);
             //$sheet->rows($data);
-            $pos=4;
-            $desglosar=array(array(),array(),array(),array(),array(),array());
+            $pos=3;
             for ($i=0; $i<count($data); $i++) {
                 $ndet=$data[$i]['ndet'];
                 $pos++;
-                unset($data[$i]['ndet']);
+                //unset($data[$i]['ndet']);
                 $data[$i]['id']=$i+1;
-                if($ndet>1){
-                    $desglosar[0]=explode("\n",$data[$i]['cursos']);
-                    $desglosar[1]=explode("\n",$data[$i]['nro_pago_c']);
-                    $desglosar[2]=explode("\n",$data[$i]['monto_pago_c']);
-                    $desglosar[3]=explode("\n",$data[$i]['nro_pago_certificado']);
-                    $desglosar[4]=explode("\n",$data[$i]['monto_pago_certificado']);
-                    $desglosar[5]=explode("\n",$data[$i]['modalidad']);
-                    $data[$i]['cursos']=$desglosar[0][0];
-                    $data[$i]['nro_pago_c']=$desglosar[1][0];
-                    $data[$i]['monto_pago_c']=$desglosar[2][0];
-                    $data[$i]['nro_pago_certificado']=$desglosar[3][0];
-                    $data[$i]['monto_pago_certificado']=$desglosar[4][0];
-                    $data[$i]['modalidad']=$desglosar[5][0];
-                }
-                $sheet->row( $pos, $data[$i] );
-                if($ndet>1){
-                    for ($j=1; $j < count($desglosar[0]); $j++) { 
-                        $pos++;
-                        $sheet->cell("Q".$pos, function($cell) use($desglosar,$j) {
-                                $cell->setValue($desglosar[5][$j]);
-                        });
-                        $sheet->cell("R".$pos, function($cell) use($desglosar,$j) {
-                                $cell->setValue($desglosar[0][$j]);
-                        });
-                        $sheet->cell("S".$pos, function($cell) use($desglosar,$j) {
-                                $cell->setValue($desglosar[1][$j]);
-                        });
-                        $sheet->cell("T".$pos, function($cell) use($desglosar,$j) {
-                                $cell->setValue($desglosar[2][$j]);
-                        });
-                        $sheet->cell("U".$pos, function($cell) use($desglosar,$j) {
-                                $cell->setValue($desglosar[3][$j]);
-                        });
-                        $sheet->cell("V".$pos, function($cell) use($desglosar,$j) {
-                                $cell->setValue($desglosar[4][$j]);
-                        });
-                    }
-                }
 
+                $sheet->row( $pos, $data[$i] );
             }
 
-            $sheet->cells('A3:'.$renturnModel['max'].'4', function($cells) {
+            $sheet->cells('A4:'.$renturnModel['max'].'4', function($cells) {
                 $cells->setBorder('solid', 'none', 'none', 'solid');
                 $cells->setAlignment('center');
                 $cells->setValignment('center');
@@ -414,14 +374,14 @@ class ReporteEM extends Controller
                 ));
             });
             
-            $sheet->setAutoSize(array(
+            $count = $sheet->getHighestRow();
+            /*$sheet->setAutoSize(array(
                 'Q', 'R','S','T','U'
             ));
 
-            $count = $sheet->getHighestRow();
 
             $sheet->getStyle('Q4:U'.$count)->getAlignment()->setWrapText(true);
-            
+            */
             $sheet->setBorder('A3:'.$renturnModel['max'].$count, 'thin');
 
         });
