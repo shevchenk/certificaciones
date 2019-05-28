4925cf3642c47b08690930d50a8fd41cd5bcae4b
shevchenk <jorgeshevchenk@gmail.com>
2019-05-28 18:39:31 -0500

    act

--- app/Models/Mantenimiento/Sucursal.php
+++ app/Models/Mantenimiento/Sucursal.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Input;
+use DB;
 
 class Sucursal extends Model
 {
