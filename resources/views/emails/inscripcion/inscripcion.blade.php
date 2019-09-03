c34fa2985e9caddaa543af7f836ce89887f0d74d
shevchenk <jorgeshevchenk@gmail.com>
2019-09-03 00:53:45 -0500

    act del email

--- resources/views/emails/inscripcion/inscripcion.blade.php
+++ resources/views/emails/inscripcion/inscripcion.blade.php
@@ -1,12 +1,88 @@
-@component('mail::message')
-# Introduction
 
-The body of your message.
+<p><strong>Ficha de Inscripci&oacute;n a Seminarios</strong></p>
+<p><strong>Ficha N&deg; 8783</strong></p>
+<p><strong>&nbsp;</strong></p>
+<p><strong>Sr. Castro Cerna, Cesar </strong></p>
+<p><strong>Egresado de nuestra universidad</strong></p>
+<p>&nbsp;</p>
+<p>Reciba usted nuestro cordial saludo a la vez expresarle mis felicitaciones por dar los pasos previos a la obtenci&oacute;n de su grado acad&eacute;micos.</p>
+<p><strong>Los seminarios a los cuales se ha inscrito, son los siguientes</strong></p>
+<table width="0">
+<tbody>
+<tr>
+<td width="302">
+<p><strong>NOMBRE DEL SEMINARIO</strong></p>
+</td>
+<td width="142">
+<p><strong>FECHA Y HORA</strong></p>
+</td>
+<td width="94">
+<p><strong>MODALIDAD</strong></p>
+<p><strong>&nbsp;</strong></p>
+</td>
+<td width="104">
+<p><strong>SOBRE EL SEMINARIO</strong></p>
+</td>
+</tr>
+<tr>
+<td width="302">
+<p>VIOLENCIA FAMILIAR</p>
+</td>
+<td width="142">
+<p>DOM 26 DE MAYO</p>
+<p>DE 10 AM A 12</p>
+</td>
+<td width="94">
+<p>PRESENCIAL</p>
+</td>
+<td width="104">
+<p>COMENTE AQU&Iacute;</p>
+</td>
+</tr>
+<tr>
+<td width="302">
+<p>SUNAT: REGIMEN TRIBUTARIO</p>
+</td>
+<td width="142">
+<p>DOM 26 DE MAYO</p>
+<p>DE 2 PM A 4 PM</p>
+</td>
+<td width="94">
+<p>VIRTUAL</p>
+</td>
+<td width="104">
+<p>COMENTE AQU&Iacute;</p>
+</td>
+</tr>
+<tr>
+<td width="302">
+<p>TECNOLOGIA DE REDES EN FIBRA OPTICA</p>
+</td>
+<td width="142">
+<p>SABADO 25 DE MAYO</p>
+<p>DE 2 PM A 4 PM</p>
+</td>
+<td width="94">
+<p>PRESENCIAL</p>
+</td>
+<td width="104">
+<p>COMENTE AQU&Iacute;</p>
+</td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
+<p><strong>Pasos que debe realizar para que hagamos entrega del certificado de asistencia al seminario:</strong></p>
+<ol>
+<li>Si el seminario es presencial, deber&aacute; asistir a la Av. Arequipa 3565, auditorio 1, primer piso. Deber&aacute; mostrar esta ficha para su ingreso, debe llegar 15 minutos antes de la hora se&ntilde;alada.</li>
+<li>Si el seminario es virtual, participe haciendo clic sobre el nombre del seminario.</li>
+<li>En ambos casos, realizar un comentario sobre el contenido del seminario, haciendo clic en COMENTE AQU&Iacute;.</li>
+<li>Recoja su certificado dos d&iacute;as despu&eacute;s de realizado su comentario, seg&uacute;n el paso 3.</li>
+</ol>
+<p>Los certificados de los seminarios se recogen en las oficinas de la Direcci&oacute;n de Extensi&oacute;n Universitaria y Responsabilidad Social, sito en la Av. Bolivia 300, de lunes a s&aacute;bados de 8:00 am a 8:00 pm, domingos de 8:00 a 2:00 pm.</p>
+<p>Cabe mencionar que el art&iacute;culo 15 del Reglamento de Grados y T&iacute;tulos, especifica que todo alumno para la obtenci&oacute;n de Grado Acad&eacute;mico de Bachiller necesita obligatoriamente asistir como m&iacute;nimo a 10 seminarios programados por la Universidad sean estos presenciales o virtuales, en caso de no hacerlo no podr&aacute; tramitar su grado acad&eacute;mico.</p>
+<p><strong>&nbsp;</strong></p>
+<p><strong>Mg. Juan Luna G&aacute;lvez</strong></p>
+<p><strong>Director</strong></p>
+<p><strong>Direcci&oacute;n de Extensi&oacute;n Universitaria y Responsabilidad Social</strong></p>
 
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
-
-Thanks,<br>
-{{ config('app.name') }}
-@endcomponent
