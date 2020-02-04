f0adaf7dafec7b604a25757ec78701ad967d022d
shevchenk <jorgeshevchenk@gmail.com>
2020-02-04 01:52:05 -0500

    act

--- app/Models/Proceso/Alumno.php
+++ app/Models/Proceso/Alumno.php
@@ -395,7 +395,7 @@ class Alumno extends Model
                     }
                 })
                 ->union($first);
-        $r = $sql->paginate(10);
+        $r = $sql->get();
 
         return $r;
     }
