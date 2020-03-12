8fff2926a9076523ee9d193e77129bef51ca4e28
shevchenk <jorgeshevchenk@gmail.com>
2020-03-12 12:02:13 -0500

    act

--- resources/views/mantenimiento/empresa/js/empresa.php
+++ resources/views/mantenimiento/empresa/js/empresa.php
@@ -176,6 +176,11 @@ ValidarTipoEvaluacion=function(){
                     '<option value="3" >3</option>'+
                     '<option value="4" >4</option>'+
                     '<option value="5" >5</option>'+
+                    '<option value="6" >6</option>'+
+                    '<option value="7" >7</option>'+
+                    '<option value="8" >8</option>'+
+                    '<option value="9" >9</option>'+
+                    '<option value="10" >10</option>'+
                 '</select></td>'+
                 '<td><a onClick="QuitarTipoEvaluacion('+$('#slct_tipo_evaluacion').val()+');" class="btn btn-flat btn-danger"><i class="fa fa-trash fa-lg"></i></a></td>'+
             '</tr>';
